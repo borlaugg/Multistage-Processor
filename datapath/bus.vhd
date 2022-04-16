6d75a6611576f5ea5648a3c4b72aa74a82a69fad
Bhavya Kohli <bhavyakohli2024@gmail.com>
2022-04-17 03:18:17 +0530

    bus

--- datapath/bus.vhd
+++ datapath/bus.vhd
@@ -3,6 +3,7 @@ use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
 entity bus is
+    generic ( data_length : integer := 16 )
     port (
         clk, rst : in std_logic;
         din      : in std_logic_vector(15 downto 0);
@@ -11,12 +12,14 @@ entity bus is
 end bus;
 
 architecture bus_arch of bus is
+variable pad := std_logic_vector(15-data_length downto 0) := (others => '0');
 begin
     process (clk, rst)
     begin
         if (rst = '1') then
             dout <= (others => '0');
         elsif (rising_edge(clk)) then
-            dout <= din;
+            dout(data_length-1 downto 0) <= din;
+            dout(15 downto data_length) <= pad;
         end if;
 end bus_arch;
