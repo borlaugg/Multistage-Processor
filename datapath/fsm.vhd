287adeeb492d97f5e0315cac1f0aeb56e3b1c2ec
Bhavya Kohli <bhavyakohli2024@gmail.com>
2022-04-17 17:47:21 +0530

    fsm modifications, ir

--- datapath/fsm.vhd
+++ datapath/fsm.vhd
@@ -10,7 +10,7 @@ package FSM is
 			inp					: in std_logic_vector(3 downto 0);
 			reset,clock			: in std_logic;
 			di, t1, cz, alu		: out std_logic_vector(1 downto 0);
-			z, cb, ao, do, inc	: out std_logic;
+			z, cb, ao, do, inc, w	: out std_logic;
 			ir, pc				: out std_logic_vector(2 downto 0);
 			mux					: out std_logic_vector(3 downto 0);
 			rf					: out std_logic_vector(4 downto 0)
@@ -30,7 +30,7 @@ entity machine is
 		inp					: in std_logic_vector(3 downto 0);
         reset,clock			: in std_logic;
         di, t1, cz, alu		: out std_logic_vector(1 downto 0);
-		z, cb, ao, do, inc	: out std_logic;
+		z, cb, ao, do, inc, w	: out std_logic;
 		ir, pc				: out std_logic_vector(2 downto 0);
 		mux					: out std_logic_vector(3 downto 0);
 		rf					: out std_logic_vector(4 downto 0)
@@ -74,6 +74,7 @@ begin
 				mux<='0000';
 				alu<='00';
 				cb<='0';
+				w<='0';
 			when s1=>
 				if(unsigned(decoded)=1) then   --k
 					y_next<=s8;
@@ -89,6 +90,7 @@ begin
 					mux<='0000';
 					alu<='00';
 					cb<='0';
+					w<='0';
 				elsif(unsigned(decoded)=2) then   
 					do<='0';
 					di<='00';
@@ -103,6 +105,7 @@ begin
 					alu<='00';
 					y_next<=s6;
 					cb<='0';
+					w<='0';
 				elsif(unsigned(decoded)=4) then   
 					do<='0';
 					di<='00';
@@ -117,6 +120,7 @@ begin
 					alu<='00';
 					y_next<=s6;
 					cb<='0';
+					w<='0';
 				elsif(unsigned(decoded)=8) then    
 					do<='0';
 					di<='00';
@@ -130,6 +134,7 @@ begin
 					mux<='0000';
 					alu<='00';
 					cb<='0';
+					w<='0';
 					y_next<=s4;
 				elsif(unsigned(decoded)=16) then   
 					do<='0';
@@ -144,6 +149,7 @@ begin
 					mux<='0000';
 					alu<='00';
 					cb<='0';
+					w<='0';
 					y_next<=s4;
 				elsif(unsigned(decoded)=32) then    
 					do<='0';
@@ -158,6 +164,7 @@ begin
 					mux<='0000';
 					alu<='00';
 					cb<='0';
+					w<='0';
 					y_next<=s11;
 				elsif(unsigned(decoded)=64) then   
 					do<='0';
@@ -172,6 +179,7 @@ begin
 					mux<='0000';
 					alu<='00';
 					cb<='0';
+					w<='0';
 					y_next<=s5;
 				elsif(unsigned(decoded)=128) then   
 					do<='0';
@@ -186,6 +194,7 @@ begin
 					mux<='0000';
 					alu<='00';
 					cb<='0';
+					w<='0';
 					y_next<=s5;
 				elsif(unsigned(decoded)=256) then   
 					do<= '00';
@@ -200,6 +209,7 @@ begin
 					mux<='0000';
 					alu<='00';
 					cb<='0';
+					w<='0';
 					y_next<=s16;
 				elsif(unsigned(decoded)=512) then   
 					do<='0';
@@ -214,6 +224,7 @@ begin
 					mux<='0000';
 					alu<='00';
 					cb<='0';
+					w<='0';
 					y_next<=s1;
 				elsif(unsigned(decoded)=1024) then   
 					do<='0';
@@ -228,6 +239,7 @@ begin
 					mux<='0000';
 					alu<='00';
 					cb<='0';
+					w<='0';
 					y_next<=s1;
 				elsif(unsigned(decoded)=2048) then   
 					do<='0';
@@ -242,6 +254,7 @@ begin
 					mux<='0000';
 					alu<='00';
 					cb<='0';
+					w<='0';
 					y_next<=s8;
 				end if;
 			when s2=>
@@ -257,6 +270,7 @@ begin
 				mux<='0000';
 				alu<='11';
 				cb<='0';
+				w<='0';
 				y_next<=s3;
 			when s3=>
 				do<='0';
@@ -271,6 +285,7 @@ begin
 				mux<='0000';
 				alu<='00';
 				cb<='0';
+				w<='0';
 				y_next<=s1;
 			when s7=>
 				do<='0';
@@ -285,6 +300,7 @@ begin
 				mux<='0000';
 				alu<='10';
 				cb<='0';
+				w<='0';
 				y_next<=s3;
 			when s8=>
 				do<='0';
@@ -299,6 +315,7 @@ begin
 				mux<='0000';
 				alu<='10';
 				cb<='0';
+				w<='0';
 				y_next<=s3;
 			when s4=>
 				if(unsigned(decode)=16) then
@@ -314,6 +331,7 @@ begin
 					mux<='0000';
 					alu<='10';
 					cb<='0';
+					w<='0';
 					y_next<=s9;
 				elsif(unsigned(decode)=8) then
 					do<='0';
@@ -328,6 +346,7 @@ begin
 					mux<='0000';
 					alu<='10';
 					cb<='0';
+					w<='0';
 					y_next<=s10;
 				end if;
 			when s9=>
@@ -343,6 +362,7 @@ begin
 				mux<='0000';
 				alu<='00';
 				cb<='0';
+				w<='0';
 				y_next<=s1;
 			when s10=>
 				do<= '1';
@@ -357,6 +377,7 @@ begin
 				mux<='0000';
 				alu<='00';
 				cb<='0';
+				w<='1';
 				y_next<=s1;
 			when s11=>
 				if(z='1') then
@@ -372,6 +393,7 @@ begin
 					mux<='0000';
 					alu<='11';
 					cb<='0';
+					w<='0';
 					y_next<=s21;
 				else 
 					do<= '00';
@@ -386,6 +408,7 @@ begin
 					mux<='0000';
 					alu<='11';
 					cb<='0';
+					w<='0';
 					y_next<=s19;
 				end if;
 			when s12=>
@@ -401,6 +424,7 @@ begin
 				mux<='0000';
 				alu<='11';
 				cb<='0';
+				w<='0';
 				y_next<=s13;
 			when s21=>
 				do<='0';
@@ -415,6 +439,7 @@ begin
 				mux<='0000';
 				alu<='11';
 				cb<='1';
+				w<='0';
 				y_next<=s13;
 			when s19=>
 				do<='0';
@@ -429,6 +454,7 @@ begin
 				mux<='0000';
 				alu<='00';
 				cb<='0';
+				w<='0';
 				y_next<=s1;
 			when s13=>
 				do<='0';
@@ -443,6 +469,7 @@ begin
 				mux<='0000';
 				alu<='00';
 				cb<='0';
+				w<='0';
 				y_next<=s1;
 			when s5=>
 				if(unsigned(decode)=128) then
@@ -458,6 +485,7 @@ begin
 					mux<='0000';
 					alu<='11';
 					cb<='0';
+					w<='0';
 					y_next<=s15;
 				elsif(unsigned(decode)=64) then
 					do<='0';
@@ -472,6 +500,7 @@ begin
 					mux<='0000';
 					alu<='11';
 					cb<='0';
+					w<='0';
 					y_next<=s14;
 				end if;
 			when s14=>
@@ -487,6 +516,7 @@ begin
 				mux<='0000';
 				alu<='11';
 				cb<='0';
+				w<='0';
 				y_next<=s13;
 			when s15=>
 				do<='0';
@@ -501,6 +531,7 @@ begin
 				mux<='0000';
 				alu<='00';
 				cb<='0';
+				w<='0';
 				y_next<=s1;
 			when s6=>
 				if (unsigned(cz)=4) then
@@ -516,6 +547,7 @@ begin
 					mux<='0000';
 					alu<='11';
 					cb<='0';
+					w<='0';
 					y_next<=s17;
 				elsif (unsigned(cz)=2) then
 					do<='0';
@@ -530,6 +562,7 @@ begin
 					mux<='0000';
 					alu<='11';
 					cb<='0';
+					w<='0';
 					y_next<=s18;
 				else
 					do<='0';
@@ -544,6 +577,7 @@ begin
 					mux<='0000';
 					alu<='11';
 					cb<='0';
+					w<='0';
 					y_next<=s20;
 				end if;
 			when s17=>
@@ -559,6 +593,7 @@ begin
 				mux<='0011';
 				alu<='00';
 				cb<='0';
+				w<='0';
 				y_next<=s1;
 			when s18=>
 				do<='0';
@@ -573,6 +608,7 @@ begin
 				mux<='0101';
 				alu<='00';
 				cb<='0';
+				w<='0';
 				y_next<=s1;
 			when s20=>
 				do<='0';
@@ -587,6 +623,7 @@ begin
 				mux<='1001';
 				alu<='00';
 				cb<='0';
+				w<='0';
 				y_next<=s1;
 			end case;
 	end process;
